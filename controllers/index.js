c90c4e2573f38099680f45ebf6d60fd5c7f497ca
Irina-Golubitsky <irina.golubitsky@gmail.com>
2021-09-13 17:40:49 -0700

    handlebars : main and homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 // import consts
 const router = require('express').Router();
 const apiRoutes = require('./api');
-//const homeRoutes = require('./home-routes.js');
-//const dashboardRoutes = require('./dashboard-routes.js');
+const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
